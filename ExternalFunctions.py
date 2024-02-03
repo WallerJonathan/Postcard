b703df66df89503a0a08635a9e53d249c5e1e359
Jonathan Waller <142193027+WallerJonathan@users.noreply.github.com>
2024-02-03 11:25:36 -0500

    Update ExternalFunctions.py

--- ExternalFunctions.py
+++ ExternalFunctions.py
@@ -8,7 +8,7 @@ def makeJson(myDict, filePath):
     try:
         with open(filePath, "w") as json_file:
             json.dump(myDict, json_file, indent=4)
-        print(f"The JSON file '{filePath}' has been created.")
+        print(f"The JSON file '{filePath}' has been created or written to.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -36,20 +36,3 @@ def randomSplitIntoDays(L):
             sublist = sublist[3:]  # Update the sublist excluding the first 3 elements
         resultList.append(newSubList)
     return resultList
-    
-# pre-conditions: takes in dictionaries d1, d2, d3, a list L that cannot exceed a length of 3, and a key.
-# post-conditions: 
-# It assigns values from the list L to the same key in the dictionaries d1, d2, and d3 based on the length of L
-# If L contains only one item, this item is assigned to d1 at the specified key.
-# # If L contains three or more items, the first item is assigned to d1 at key, the second item to d2 at the same key, 
-# and the third item to d3 at the same key.
-def addItin(d1, d2, d3, L, key):
-    if len(L) == 1:
-        d1[key] = L[0]
-    elif len(L) == 2:
-        d1[key] = L[0]
-        d2[key] = L[1]
-    else:
-        d1[key] = L[0]
-        d2[key] = L[1]
-        d3[key] = L[2]
